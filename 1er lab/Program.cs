6ea90de8bdb383d405d71d82ba33ecfe1d203d0c
MoonStalker11 <122495480+MoonStalker11@users.noreply.github.com>
2024-03-28 16:41:22 -0400

    V1

--- 1er lab/Program.cs
+++ 1er lab/Program.cs
@@ -6,38 +6,45 @@ namespace ClaseEntero
     {
         public static void Main(string[] args)
         {
-            // Ejercicio 1: Rotar un dígito del número ingresado por teclado
-            Console.WriteLine("Ejercicio 1:");
-            Console.WriteLine("Ingrese un número entero:");
-            int numero = Convert.ToInt32(Console.ReadLine());
-            Entero entero = new Entero(numero);
-            int nuevoNumero = entero.RotarUltimoDigito();
-            Console.WriteLine("Número rotado: " + nuevoNumero);
+            Console.WriteLine("Ingrese el tamaño de la matriz (filas columnas):");
+            string[] tamanoInput = Console.ReadLine().Split(' ');
 
-            // Ejercicio 2: Verificar si todos los dígitos de un número ingresado por teclado están ordenados ascendentemente
-            Console.WriteLine("\nEjercicio 2:");
-            Console.WriteLine("Ingrese un número entero:");
-            numero = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Ingrese la cantidad de dígitos a comparar:");
-            int nd = Convert.ToInt32(Console.ReadLine());
-            bool ascendente = entero.DigitsAscendentes(nd);
-            Console.WriteLine("Los dígitos están ordenados ascendentemente: " + ascendente);
+            int filas = int.Parse(tamanoInput[0]);
+            int columnas = int.Parse(tamanoInput[1]);
 
-            // Ejercicio 3: Verificar cuántas vocales tiene una palabra ingresada por teclado
-            Console.WriteLine("\nEjercicio 3:");
-            Console.WriteLine("Ingrese una palabra:");
-            string palabra = Console.ReadLine();
-            Cadena cadena = new Cadena(palabra);
-            int contadorVocales = cadena.ContarVocales();
-            Console.WriteLine("La palabra tiene " + contadorVocales + " vocales.");
+            int[,] matrizGenerada = GenerarMatrizPatron(filas, columnas);
 
-            // Ejercicio 4: Contar cuántas palabras tiene una oración ingresada por teclado
-            Console.WriteLine("\nEjercicio 4:");
-            Console.WriteLine("Ingrese una oración:");
-            string oracion = Console.ReadLine();
-            cadena = new Cadena(oracion);
-            int contadorPalabras = cadena.ContarPalabras();
-            Console.WriteLine("La oración tiene " + contadorPalabras + " palabras.");
+            MostrarMatriz(matrizGenerada);
+        }
+
+        static int[,] GenerarMatrizPatron(int filas, int columnas)
+        {
+            int[,] matriz = new int[filas, columnas];
+
+            for (int i = 0; i < filas; i++)
+            {
+                for (int j = 0; j < columnas; j++)
+                {
+                    matriz[i, j] = (i + 1) * (j + 1);
+                }
+            }
+
+            return matriz;
+        }
+
+        static void MostrarMatriz(int[,] matriz)
+        {
+            int filas = matriz.GetLength(0);
+            int columnas = matriz.GetLength(1);
+
+            for (int i = 0; i < filas; i++)
+            {
+                for (int j = 0; j < columnas; j++)
+                {
+                    Console.Write(matriz[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
